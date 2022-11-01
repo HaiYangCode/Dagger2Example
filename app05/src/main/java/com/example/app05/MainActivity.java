48df12305d5c886bdd156fcb30651c95d23c1d02
HaiYangCode <kk@gmail.com>
2022-11-01 21:37:01 +0800

    dev:使用Lazy<T>

--- app05/src/main/java/com/example/app05/MainActivity.java
+++ app05/src/main/java/com/example/app05/MainActivity.java
@@ -7,12 +7,16 @@ import android.util.Log;
 
 import javax.inject.Inject;
 
+import dagger.Lazy;
 import dagger.multibindings.IntoMap;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
     BInterface impl01;
+
+    @Inject
+    Lazy<BInterface> impl02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,9 @@ public class MainActivity extends AppCompatActivity {
                 .inject(this);
 
         Log.i("TAG", "impl01 === " + impl01.getClass().getSimpleName());
+
+        Log.i("TAG", "impl02 === " + impl02.getClass().getSimpleName());
+
+        Log.i("TAG", "impl02 impl02.get()=== " + impl02.get().getClass().getSimpleName());
     }
 }
